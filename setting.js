a4243835cc815a76983f441ef583bbe7580825bf
caizhen0110 <130143594+caizhen0110@users.noreply.github.com>
2023-11-22 15:09:10 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,3 +1,3 @@
-﻿var FHIRrootURL ="https://hapi.fhir.org/baseR4";
+﻿var FHIRrootURL ="https://hapi.fhir.org/baseR4/";
 /* https://fhir.tcumi.com:58443/r5/fhir/ */
 /* https://hapi.fhir.org/baseR4/Patient */
